241900b42dfde7ffa79858f0d51460dd3b8c1c2d
jyw <jyw1341@gmail.com>
2022-07-07 17:51:20 +0900

    0707

---
+++ src/test/java/jyw/core/findbean/ApplicationConfigExtendsFindTest.java
@@ -0,0 +1,72 @@
+package jyw.core.findbean;
+
+import jyw.core.discount.DiscountPolicy;
+import jyw.core.discount.FixeDiscountPolicy;
+import jyw.core.discount.RateDiscountPolicy;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ApplicationConfigExtendsFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+    @Configuration
+    static class TestConfig {
+
+        @Bean
+        public DiscountPolicy rateDiscountPolicy() {
+            return new RateDiscountPolicy();
+        }
+
+        @Bean
+        public DiscountPolicy fixDiscountPolicy() {
+            return new FixeDiscountPolicy();
+        }
+
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 조회시 자식이 둘 이상 있으면 중복 오류가 발생한다")
+    void findByParentTypeDuplicate() {
+        Assertions.assertThrows(NoUniqueBeanDefinitionException.class, () -> ac.getBean(DiscountPolicy.class));
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 조회시 자식이 둘 이상 있으면 빈 이름을 지정하자")
+    void findByParentTypeBeanName() {
+        DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
+        assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("특정 하위 타입으로 조회")
+    void findBySubType() {
+        DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", RateDiscountPolicy.class);
+        assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기")
+    void findAllBeansByParentType() {
+        Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
+        assertThat(beansOfType.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기 - object")
+    void findAllBeansByPType() {
+        Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
+        for (String s : beansOfType.keySet()) {
+            System.out.println("s = " + s);
+        }
+    }
+}
